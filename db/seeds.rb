4d3e51216e39aab5b8a85b9cdc710bdc82d7108e
ronaldcnunez <Ronaldcuello11@gmail.com>
2018-11-06 13:07:50 -0500

    DONEZO

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@ Venue.delete_all
 
 #TEST ACCOUNTS
 Artist.create(
-  name:"Test",
+  name:"Artist Tester",
   email: "test@gmail.com",
   description:Faker::ChuckNorris.fact,
   genre: "TEST",
   img_url: "https://www.telegraph.co.uk/content/dam/news/2017/06/26/TELEMMGLPICT000088211244_trans_NvBQzQNjv4BqrduRQfIjONMjRT2xVSqTqUSSKLtb6W3JTHO22Hljeu4.jpeg?imwidth=900",
   password: "test1234")
 
-  Venue.create(name: "Test",
+  Venue.create(name: "Venue Tester",
     email: "test1@gmail.com",
     description: Faker::MichaelScott.quote,
     location: "New York, New York",
@@ -130,5 +130,17 @@ Artist.create(
 Event.create(
   artist_id: 1,
   venue_id: 1,
+  description: Faker::ChuckNorris.fact,
+  date: Faker::Date.forward(45))
+
+Event.create(
+  artist_id: 1,
+  venue_id: 2,
+  description: Faker::ChuckNorris.fact,
+  date: Faker::Date.forward(45))
+
+Event.create(
+  artist_id: 1,
+  venue_id: 3,
   description: Faker::ChuckNorris.fact,
   date: Faker::Date.forward(45))
